455af7bc047358058f2408179b09deee147a378c
Casey Kelly <casey.kelly@colorado.edu>
2021-04-01 17:50:50 -0600

    create object of purchased items and quantity

--- price_calculator.js
+++ price_calculator.js
@@ -10,8 +10,16 @@ const PRICES = {
   bread: { unitPrice: 2.17, salePrice: 6, saleQuantity: 3 },
   banana: { unitPrice: 0.99 },
   apple: { unitPrice: 0.89 }
+};
+
+const countPurchasedItems = (items) => {
+  let itemCount = {};
+  const itemArray = items.split(',');
+  itemArray.forEach((item) => { itemCount[item.trim()] = (itemCount[item.trim()] || 0) + 1 });
+  return itemCount;
 }
 
 rl.question('Please enter all the items purchased separated by a comma: ', (items) => {
+  const itemCount = countPurchasedItems(items);
   rl.close();
 });
