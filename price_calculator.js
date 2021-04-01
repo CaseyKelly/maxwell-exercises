564e47c7d52b2bb2a9e321434ff0027dd2c5e99f
Casey Kelly <casey.kelly@colorado.edu>
2021-04-01 17:27:57 -0600

    add price data

--- price_calculator.js
+++ price_calculator.js
@@ -5,6 +5,13 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const PRICES = {
+  milk: { unitPrice: 3.97, salePrice: 5, saleQuantity: 2 },
+  bread: { unitPrice: 2.17, salePrice: 6, saleQuantity: 3 },
+  banana: { unitPrice: 0.99 },
+  apple: { unitPrice: 0.89 }
+}
+
 rl.question('Please enter all the items purchased separated by a comma: ', (items) => {
   rl.close();
 });
