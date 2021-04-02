5257575a190095b4ff14df1776557ae7dba25d2a
Casey Kelly <casey.kelly@colorado.edu>
2021-04-01 18:44:49 -0600

    determine prices for purchased items

--- price_calculator.js
+++ price_calculator.js
@@ -12,14 +12,44 @@ const PRICES = {
   apple: { unitPrice: 0.89 }
 };
 
+const calculatePrices = (itemCount) => {
+  const prices = {};
+  const items = Object.keys(itemCount);
+  for (item of items) {
+    const quantity = itemCount[item];
+    prices[item] = { quantity, price: determinePrice(item, quantity) };
+  }
+  return prices;
+}
+
 const countPurchasedItems = (items) => {
   let itemCount = {};
-  const itemArray = items.split(',');
-  itemArray.forEach((item) => { itemCount[item.trim()] = (itemCount[item.trim()] || 0) + 1 });
+  const itemArray = items.split(',').map((item) => item.trim());
+  itemArray.forEach((item) => { itemCount[item] = (itemCount[item] || 0) + 1 });
   return itemCount;
+}
+
+const determinePrice = (item, quantity) => {
+  let price = 0;
+  const saleQuantity = PRICES[item].saleQuantity;
+  const unitPrice = PRICES[item].unitPrice;
+  const salePrice = PRICES[item].salePrice;
+
+  if (saleQuantity && (quantity >= saleQuantity)) {
+    const fullPriceItems = quantity % saleQuantity;
+    const saleItems = Math.floor(quantity / saleQuantity);
+    price += ((saleItems * salePrice) + (fullPriceItems * unitPrice))
+  } else {
+    price += (unitPrice * quantity);
+  }
+  return price;
 }
 
 rl.question('Please enter all the items purchased separated by a comma: ', (items) => {
   const itemCount = countPurchasedItems(items);
+  const itemPrices = calculatePrices(itemCount);
+
+  console.log(itemPrices);
+
   rl.close();
 });
